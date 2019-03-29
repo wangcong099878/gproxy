daf0f761d8d1841f4290cc96d59cad83e42e7b77
wangcong <251957448@qq.com>
2019-03-29 15:49:36 +0800

    all

--- index.js
+++ index.js
@@ -6,13 +6,55 @@ var proxy = require('http-proxy-middleware')
 
 var jsonPlaceholderProxy = proxy([
     '/api',
-    '/ajax',
-    '/someotherpath'
+    '/adminapi',
+    '/other'
 ], {
     target: 'http://local.wbwan.com',
     changeOrigin: true,
     logLevel: 'debug'
 })
+
+///browserSync.watch('*.html').on('change', browserSync.reload);  'page/*/*/*'
+browserSync.watch(['.'], {ignored: [
+    'node_modules',
+    '.git',
+    '.idea',
+    '.gitignore'
+]}).on('all', (event, path) => {
+    //console.log(event, path);
+    browserSync.reload();
+});
+
+//定向匹配
+// browserSync.watch(['static/*/*/*','page/.*'], {ignored: [
+//     'node_modules',
+//     '.git',
+//     '.idea',
+//     '.gitignore'
+// ]}).on('all', (event, path) => {
+//     console.log(event, path);
+//     browserSync.reload();
+// });
+
+// browserSync.watch('*.css',function(event, file){
+//     console.log(event);
+//     console.log(file);
+//     browserSync.reload();
+// });
+// browserSync.watch('*.css',function(event, file){
+//     console.log(event);
+//     console.log(file);
+//     browserSync.reload();
+// });
+
+// browserSync.watch("static/*", function (event, file) {
+//     console.log(event);
+//     if (event === "change") {
+//         browserSync.reload("*.css");
+//     }
+// });
+
+//browserSync.watch('*.js').on('change', browserSync.reload);
 
 browserSync.init({
     server: {
@@ -22,6 +64,8 @@ browserSync.init({
     },
     startPath: '/index.html'
 })
+
+
 
 console.log('[GPROXY] Server: listening on port 3000')
 console.log('[GPROXY] Opening: http://localhost:3000/index.html')
